5e790a0126e255be1151033a371deab6554e115c
Craig Robinson <craig@absoluteherogames.com>
2015-06-17 13:46:12 -0700

    fix for haxe 3.2

--- format/amf3/Reader.hx
+++ format/amf3/Reader.hx
@@ -113,7 +113,7 @@ class Reader {
 			len >>= 1;
 			var s = "";
 
-		var c = 0, d = 0, j = 0, it = 0;
+		var c = 0, d = 0, j:UInt = 0, it = 0;
 		while (j < len) {
 			c = i.readByte();
 			if (c < 0x80) {
